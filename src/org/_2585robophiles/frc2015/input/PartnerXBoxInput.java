0c3bae81c7c2f85c4ea03a17ddc7c6b959c985d6
XRuizhe <ruizhe1997@gmail.com>
2015-01-09 10:26:43 -0600

    Update PartnerXBoxInput.java

--- src/org/_2585robophiles/frc2015/input/PartnerXBoxInput.java
+++ src/org/_2585robophiles/frc2015/input/PartnerXBoxInput.java
@@ -3,27 +3,27 @@ package org._2585robophiles.frc2015.input;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class PartnerXBoxInput implements InputMethod{
-  /*
-1: A - outtake
-2: B - shift
-3: X - intake
-4: Y - shooter pneumatic
-5: Left Bumper - shoot
-6: Right Bumper - shift
-7: Back - straight drive
-8: Start
-9: Left Joystick
-10: Right Joystick
-*/
+    /*
+    1: A - outtake
+    2: B - shift
+    3: X - intake
+    4: Y - shooter pneumatic
+    5: Left Bumper - shoot
+    6: Right Bumper - shift
+    7: Back - straight drive
+    8: Start
+    9: Left Joystick
+    10: Right Joystick
+     */
 
-private Joystick driver;
-private Joystick shooter;
-    
+    private Joystick driver;
+    private Joystick shooter;
+
     public PartnerXboxInput() {
         driver = new Joystick(1);
         shooter = new Joystick(2);
     }
-    
+
     public double getLeftX() {
         return driver.getRawAxis(XboxInput.LEFT_X_AXIS);
     }
@@ -39,49 +39,46 @@ private Joystick shooter;
     public double getRightY() {
         return driver.getRawAxis(XboxInput.RIGHT_Y_AXIS);
     }
-    
+
     public boolean shift() {
         return driver.getRawButton(XboxInput.B_BUTTON) || driver.getRawButton(XboxInput.RIGHT_BUMPER);
     }
-    
+
     public boolean catchBall() {
         return driver.getRawButton(XboxInput.A_BUTTON);
     }
-    
+
     public boolean shoot() {
         return shooter.getRawButton(XboxInput.LEFT_BUMPER);
     }
-    
+
     public boolean intakeLift() {
         return shooter.getRawButton(XboxInput.B_BUTTON);
     }
-    
+
     public boolean shooterLift() {
         return shooter.getRawButton(XboxInput.Y_BUTTON);
     }
-    
-    /* (non-Javadoc)
-     * @see com.bellaire.aerbot.input.InputMethod#directionToggle()
-     */
+
     public boolean directionToggle() {
         return driver.getRawButton(XboxInput.Y_BUTTON) || driver.getRawButton(XboxInput.RIGHT_BUMPER);
     }
-    
+
     public boolean intakeToggle(){
         return shooter.getRawButton(XBoxInput.RIGHT_BUMPER);
     }
-    
+
     public int intake(){
-      if(shooter.getRawButton(XBoxInput.A_BUTTON)){
-        return -1;
-      }else if(shooter.getRawButton(XBoxInput.X_BUTTON)){
-        return 1;
-      }else{
-        return 0;
-      }
+        if(shooter.getRawButton(XBoxInput.A_BUTTON)){
+            return -1;
+        }else if(shooter.getRawButton(XBoxInput.X_BUTTON)){
+            return 1;
+        }else{
+            return 0;
+        }
     }
-    
+
     public boolean straightDrive(){
-      return driver.getRawButton(XBoxInput.BACK_BUTTON);
+        return driver.getRawButton(XBoxInput.BACK_BUTTON);
     }
 }
