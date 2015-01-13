b6936bb755f55d6266a85df7caae5d6717ba3bfe
XRuizhe <ruizhe1997@gmail.com>
2015-01-13 17:40:43 -0600

    Update PartnerXBoxInput.java

--- src/org/_2585robophiles/frc2015/input/PartnerXBoxInput.java
+++ src/org/_2585robophiles/frc2015/input/PartnerXBoxInput.java
@@ -1,5 +1,6 @@
 package org._2585robophiles.frc2015.input;
 
+import org._2585robophiles.lib2585.XboxConstants;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class PartnerXBoxInput implements InputMethod{
